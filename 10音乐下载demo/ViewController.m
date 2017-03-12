5a4ba348109b11dccf68e678c55ad4ecdac626e0
teachertu <tuzeyu@itcast.cn>
2017-03-12 17:00:28 +0800

    Initial Commit

---
+++ 10音乐下载demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  10音乐下载demo
+//
+//  Created by HM09 on 17/3/12.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ 10音乐下载demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  10音乐下载demo
+//
+//  Created by HM09 on 17/3/12.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
